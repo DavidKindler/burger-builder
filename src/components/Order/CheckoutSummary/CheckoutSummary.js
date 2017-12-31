ac0ee1404c0e10d67c705f629b9c01fc0d8be76d
David Kindler <david.kindler@nxp.com>
2017-12-30 20:34:36 -0600

    checkout summary, add router

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import classes from './CheckoutSummary.css';
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+
+const CheckoutSummary = props => {
+  return (
+    <div className={classes.CheckoutSummary}>
+      <h1>We hope it tastes great</h1>
+      <div style={{ width: '100%', margin: 'auto' }}>
+        <Burger ingredients={props.ingredients} />
+      </div>
+      <Button clicked btnType="Danger">
+        CANCEL
+      </Button>
+      <Button clicked btnType="Success">
+        CONTINUE
+      </Button>
+    </div>
+  );
+};
+export default CheckoutSummary;
